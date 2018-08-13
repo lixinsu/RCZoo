c44469735e47b1421ebf37996ca9c53759b82c09
lixinsu <sulixinict@gmail.com>
2018-08-13 19:12:38 +0800

    [feature]add predict script for chn dataset

---
+++ predict.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PYTHONPATH=`pwd`:$PYTHONPATH
+
+DATA=test
+python3 scripts/reader/converter.py data/sogou/${DATA}.json data/sogou/${DATA}_std_all.json test
+python3 scripts/reader/predict.py data/sogou/${DATA}_std_all.json --model data/models/docqa_sogou.mdl --tokenizer jieba --out-dir data/sogou
+python3 scripts/reader/merge_result.py data/sogou/${DATA}_std_all-docqa_sogou.preds
+
