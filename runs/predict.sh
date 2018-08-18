5cf4e83725b1e23e46e6a10938bf12a5668b0f29
su <sulixinict@gmail.com>
2018-08-18 11:50:09 +0800

    Dev (#19)
    * [feature]fix OOM of QAnet
    
    * [feature]move bash scripts into folder

--- runs/predict.sh
+++ runs/predict.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PYTHONPATH=`pwd`:$PYTHONPATH
-
+cd `pwd`
 DATA=test
 python3 scripts/reader/converter.py data/sogou/${DATA}.json data/sogou/${DATA}_std_all.json test
 python3 scripts/reader/predict.py data/sogou/${DATA}_std_all.json --model data/models/docqa_sogou.mdl --tokenizer jieba --out-dir data/sogou
