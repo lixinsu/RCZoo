da04ba3644bb0384d3be2003a98df0c5343560d9
lixinsu <sulixinict@gmail.com>
2018-10-16 16:37:56 +0800

    Modify the training scripts

--- runs/train_squad.sh
+++ runs/train_squad.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 set -ex
 export PYTHONPATH=`pwd`:$PYTHONPATH
+
 MODEL=$1
 
-#python3 scripts/reader/preprocess.py data/datasets/ data/datasets/ --split SQuAD-v1.1-train --tokenizer spacy
-#python3 scripts/reader/preprocess.py data/datasets/ data/datasets/ --split SQuAD-v1.1-dev --tokenizer spacy
+models="bidaf drqa slqa fusionnet docqa qanet"
+valid_model=false
+for m in $models
+do
+    echo $m
+    if [ "$m" = "$MODEL" ]; then
+        valid_model=true
+    fi
+done
+if [ "$valid_model" = true ] ; then
+    echo "train the model $MODEL"
+else
+    echo "The $MODEL model is not supported now"
+    exit 1
+fi
+
+python3 scripts/reader/preprocess.py data/datasets/ data/datasets/ --split SQuAD-v1.1-train --tokenizer spacy
+python3 scripts/reader/preprocess.py data/datasets/ data/datasets/ --split SQuAD-v1.1-dev --tokenizer spacy
 
 python3 scripts/reader/train_${MODEL}.py --data-dir data/datasets \
                                         --restrict-vocab 0 --train-file SQuAD-v1.1-train-processed-spacy.txt \
